907d5efcec646dbc3ab7dd4ca9fd5fef0d831e69
Bernardo Fonseca <bernardo1411@hotmail.com>
2022-01-14 12:25:51 -0300

    Update header.jsx

--- hash/src/components/header.jsx
+++ hash/src/components/header.jsx
@@ -4,7 +4,7 @@ import "./header.css";
 export default (props) => (
   <div className="header">
     {props.end ? (
-      <h1>{props.name ? "O" : "X"} venceu!</h1>
+      <h1>{props.name ? "O" : "X"} won!</h1>
     ) : (
       <h1> Player: {props.name ? "X" : "O"}</h1>
     )}
